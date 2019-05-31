daa33dd0faeb81b24a9d0fabbec9413664886fba
Jeremy Carter <jeremy@jeremycarter.ca>
2019-05-31 11:34:25 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
 )
 
 func main() {
-    resp, err := http.Get("https://eternalvoid.net")
-    check(err)
-    body, err := ioutil.ReadAll(resp.Body)
-    check(err)
-    fmt.Println(len(body))
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	resp, err := http.Get("https://eternalvoid.net")
+	check(err)
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+	fmt.Println(len(body))
+
+	wg.Wait()
 }
 
 func check(err error) {
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
